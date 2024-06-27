e568f47d20adb1eb6d19d5463a62f768bbfbccf5
alokjiten <jitenalok03@gmail.com>
2024-06-27 20:21:56 +0530

    PythonTask1.2

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -1,3 +1,6 @@
+#Q2- Conditional Statements
+
+#Python Code-
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
@@ -8,6 +11,13 @@ elif(b >= c):
     print("Second number is largest", b)
 else:
     print("Third is largest", c)
+
+
+#Output
+# Enter first number: 10
+# Enter second number: 15
+# Enter third number: 20
+# Third is largest 20
 
 
 
