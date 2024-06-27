1dd09f80ac84662cd004b8bb35106e465291d9db
alokjiten <jitenalok03@gmail.com>
2024-06-27 20:17:57 +0530

    PythonTask1.1

---
+++ ArithmetricOperations.py
@@ -0,0 +1,20 @@
+# Q1 Arithmetric Operations
+
+#Python code-
+a = 7
+b = 4
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+#Output
+# 11
+# 3
+# 28
+# 1.75
+# 3
+# 2401
