ec6a772c963cbe3436222406801e60e769e6f325
alokjiten <jitenalok03@gmail.com>
2024-06-29 06:54:46 +0530

    PythonTask1.3

---
+++ TupleOperations.py
@@ -0,0 +1,20 @@
+#Q3- Tuple 
+
+#Python Code-
+tup = (1, 2, 3, "hello")
+
+print(tup[0])
+print(tup[3])
+
+new_tup = tup + (4, 5)
+print(new_tup)
+
+
+#Output
+# 1
+# hello
+# (1, 2, 3, 'hello', 4, 5)
+
+
+
+
